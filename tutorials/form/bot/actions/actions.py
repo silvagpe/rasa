da51139584a6777a1d9c3827ad7f26a9e5c48fbf
Rafael Da Silva <silvagpe@gmail.com>
2021-05-12 14:49:24 -0300

    exemplo form simple

--- tutorials/form/bot/actions/actions.py
+++ tutorials/form/bot/actions/actions.py
@@ -7,45 +7,21 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-from typing import Any, Text, Dict, List
-
-from rasa_sdk.events import SlotSet
-from rasa_sdk import Action, Tracker
-from rasa_sdk.executor import CollectingDispatcher
-
-
-class ActionReceiveName(Action):
-
-    def name(self) -> Text:
-        return "action_receive_name"
-
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        print("action_receive_name")
-        text = tracker.latest_message['text']
-        print(text)
-        dispatcher.utter_message(text=f"I'll remember your name {text}!")
-        return [SlotSet("name", text)]
-
-
-class ActionSayName(Action):
-
-    def name(self) -> Text:
-        return "action_say_name"
-
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
-        print("action_say_name")
-        name = tracker.get_slot("name")
-        print(name)
-        if not name:
-            dispatcher.utter_message(text="I don't know your name.")
-        else:
-            dispatcher.utter_message(text=f"Your name is {name}!")
-        return []
-
-
-
+# from typing import Any, Text, Dict, List
+#
+# from rasa_sdk import Action, Tracker
+# from rasa_sdk.executor import CollectingDispatcher
+#
+#
+# class ActionHelloWorld(Action):
+#
+#     def name(self) -> Text:
+#         return "action_hello_world"
+#
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#
+#         dispatcher.utter_message(text="Hello World!")
+#
+#         return []
