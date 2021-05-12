79606007a2ee0c0a4bb3ee58715f3bddeca9f4c6
Rafael Da Silva <silvagpe@gmail.com>
2021-05-12 15:57:28 -0300

    teste api

--- udemy_sufa/project2/bot/api.py
+++ udemy_sufa/project2/bot/api.py
@@ -12,6 +12,7 @@ app.config['JSON_SORT_KEYS'] = False
 # Creating appi instance
 api = Api(app)
 
+
 class rasa(Resource):
     def get(self):
         return jsonify({"message": "Api rasa. utilize envio de mensagens por POST"})
@@ -19,7 +20,7 @@ class rasa(Resource):
     def post(self):
 
         if (request.get_json() == None):
-            return Response( "No content", status=204, mimetype='application/json')
+            return Response("No content", status=204, mimetype='application/json')
 
         id = request.get_json()['id']
         msg = request.get_json()['msg']
@@ -40,4 +41,4 @@ class rasa(Resource):
 api.add_resource(rasa, '/chat')
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0")
